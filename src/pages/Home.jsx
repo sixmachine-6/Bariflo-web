402b3d7bc17e1788230ea0ec2ec1b100cdfe6a96
Aman Shaw <shawaman2384@gmail.com>
2024-06-18 15:05:51 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,35 +12,29 @@ import Investor from "../components/Investor";
 import Leadership from "../components/Leadership";
 import Spotlight from "../components/Spotlight";
 import Award from "../components/Award";
-import { Element, Link } from "react-scroll";
-import { IoMenuOutline } from "react-icons/io5";
-import { useState } from "react";
 
 function Home() {
-  const [showNav, setShowNav] = useState(false);
   return (
     <>
       <Header />
       <Title />
       <Video />
       <SubVideo />
-      <Element name="about">
-        <About />
-      </Element>
+
+      <About />
 
       {/* <Service /> */}
-      <Element name="what-we-do">
-        <WhatWeDo />
-      </Element>
+
+      <WhatWeDo />
+
       <Support />
       {/* <Investor /> */}
       {/* <Award />
       <Spotlight />
       <Leadership /> */}
 
-      <Element name="contact">
-        <Form />
-      </Element>
+      <Form />
+
       <Contact />
       <Footer />
     </>
